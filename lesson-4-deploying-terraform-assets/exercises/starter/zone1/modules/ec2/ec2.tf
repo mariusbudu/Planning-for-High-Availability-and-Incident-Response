bc99cd91e191a1c20c56619e4dd584f55eb61b7b
Marius Budu <marius.budu@vodafone.com>
2023-10-11 22:47:55 +0300

    changes for lesson 4 zone 1

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -1,15 +1,43 @@
 resource "aws_instance" "ubuntu" {
-  ami           = 
-  count = 
-  instance_type = 
+  ami           = var.aws_ami
+  count = var.instance_count
+  instance_type = "t3.micro"
   subnet_id = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name = ""
+    Name = "Ubuntu-EC2"
   }
 }
 
 resource "aws_security_group" "ec2_sg" {
- 
+  name = "EC2 Security Group"
+  description = "Allow inbound traffic"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "HTTP from everywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH from everywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
 }
