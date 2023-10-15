04102dea80c1db9e7a6ce2d101f87407638ec847
Marius Budu <marius.budu@vodafone.com>
2023-10-15 17:48:58 +0300

    all details for lesson 4

--- lesson-4-deploying-terraform-assets/demo/zone2/ec2.tf
+++ lesson-4-deploying-terraform-assets/demo/zone2/ec2.tf
@@ -1,6 +1,6 @@
   module "project_ec2" {
    source             = "./modules/ec2"
    instance_count     = 2
-   aws_ami            = "ami-0e0bf4b3a0c0e0adc"
+   aws_ami            = "ami-0ac3c7153de5be64a"
    public_subnet_ids  = module.vpc.public_subnet_ids
  }
